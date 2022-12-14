f164eb8b073767c71f67ed93776a022de1e7bac3
Kinshi-d <omobolanleomolere@gmail.com>
2022-12-13 20:40:43 -0800

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+
+	_putchar('\n');
+}
