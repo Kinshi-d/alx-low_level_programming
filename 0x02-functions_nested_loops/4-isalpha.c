d95cfc192608a8206417eb4bb6e3df4324f12bc3
Kinshi-d <omobolanleomolere@gmail.com>
2022-12-14 19:26:15 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,10 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') ||
-			((c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || ((c >= 'A' && c <= 'Z'))
+
 			return (0);
 	else
+
 	return (0);
 }
