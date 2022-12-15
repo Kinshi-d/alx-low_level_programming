99406756e1761f5076360694309eef4fee25ba2a
Kinshi-d <omobolanleomolere@gmail.com>
2022-12-14 19:59:48 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * prints_alphabet_x10 - prints lowercase  alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char num, letter;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (letter = a 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
