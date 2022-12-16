62ddaa30ef2dcbf45e5931bc874c023c5eac5306
Kinshi-d <omobolanleomolere@gmail.com>
2022-12-15 23:11:34 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 int i, j;
 
-for (i = 0; i < 10; i++)
+for (i = 1; i <= 10; i++)
 {
-	for (j = 0; j < 15; j++)
+	for (j = 0; j <= 14; j++)
 	{
 		if (j >= 10)
 			_putchar('1');
@@ -19,4 +19,4 @@ for (i = 0; i < 10; i++)
 	}
 	_putchar('\n')
 }
-i}
+}
