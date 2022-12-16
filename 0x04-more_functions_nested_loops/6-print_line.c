4cf7ddc4a11f8f5dc4e21f7a7823141e73da59e7
Kinshi-d <omobolanleomolere@gmail.com>
2022-12-15 23:42:01 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,6 +3,7 @@
 /**
  * print_line - draw a straight line
  * @n: number of times the character _ should be printed
+ * Return: void
  */
 
 void print_line(int n)
@@ -11,7 +12,7 @@ void print_line(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(_);
+		_putchar('_');
 	}
 	_putchar('\n')
 }
