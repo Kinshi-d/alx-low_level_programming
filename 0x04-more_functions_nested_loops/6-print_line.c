ed4645f3a9a4928c6045d20237b89d171146009a
Kinshi-d <omobolanleomolere@gmail.com>
2022-12-15 23:33:26 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,18 +7,11 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n')
-	}
-	else
-	{
-		int i;
+	int i;
 
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_')
-		}
-		_putchar('\n')
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(95);
 	}
+	_putchar('\n')
 }
