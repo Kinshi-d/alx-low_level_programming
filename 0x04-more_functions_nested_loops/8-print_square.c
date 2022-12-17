3c99eb09e8500d7353299091867725eb2a00503f
Kinshi-d <omobolanleomolere@gmail.com>
2022-12-16 19:38:07 -0800

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,14 +11,20 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < (size); j++)
-		{
-			_putchar(35);
-		}
-		if (i != size - 1)
+	j = 0;
+
+	if (size < 1)
 		_putchar('\n');
+
+	while (j < size)
+	{
+		i = 0;
+		while (i < size)
+		{
+			_putchar('#');
+			i++;
+		}
+		_putchar('\n');
+		j++;
 	}
-	_putchar('\n');
 }
