1c6da0fd82dccfe531632316f619edfe950d3f75
Kinshi-d <omobolanleomolere@gmail.com>
2022-12-20 20:57:15 -0800

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -30,7 +30,7 @@ int main(void)
 		diff_half2 = (sum - 2772) / 2;
 
 		if ((sum - 2772) % 2 != 0)
-			diff_half1++
+			diff_half1++;
 				for (index = 0; password[index]; index++)
 				{
 					if (password[index] >= (33 + diff_half1))
