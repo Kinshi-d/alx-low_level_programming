2343e53a4374968f26c3c9f5f40046bbc7a00447
Kinshi-d <omobolanleomolere@gmail.com>
2022-12-23 18:25:42 -0800

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,27 +6,17 @@
  * Return: Always 0
  */
 void print_number(int n)
+
 {
-	unsigned int m, d, count;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-
-		_putchar(45);
-		m = n * -1;
+		_putchar('-');
+		num = -num;
 	}
 
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
