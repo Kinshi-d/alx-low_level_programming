6ddf74306a1ab005a311b6783d60763642a67f3f
Kinshi-d <omobolanleomolere@gmail.com>
2023-01-03 17:36:06 -0800

    memset

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,8 +17,4 @@ int main(void)
 	 * - only one stateent
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
-}
+	p[5]
